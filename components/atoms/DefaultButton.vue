4f4bb03bae8e46f2a61e0c5956cb5099c07ee88e
takupeso <takuma.abe4002@gamil.com>
2019-03-16 20:38:47 +0900

    ざっくり作成

---
+++ components/atoms/DefaultButton.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <input type="button" class="bg-main-color hover:bg-green-lighter text-white font-bold py-2 px-4 rounded" :value="text">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PortfolioSearchBox',
+  components: {
+  },
+  props: {
+    text: String
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+/* label{
+  padding:5px;
+  border: 1px solid plum;
+  border-radius: 5px;
+}
+input{
+  display: none;
+}
+.check_box:checked + .label {
+  background-color: orange;
+} */
+</style>
---
+++ components/atoms/MultiSelectButton.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <input type="checkbox" class="hidden" id="btn" />
+      <label class="block bg-transparent text-grey hover:text-main-color border border-grey-dark hover:border-main-color py-1 px-3 rounded-full" for="btn">{{text}}</label>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PortfolioSearchBox',
+  components: {
+  },
+  props: {
+    text: String
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+/* label{
+  padding:5px;
+  border: 1px solid plum;
+  border-radius: 5px;
+}
+input{
+  display: none;
+}
+.check_box:checked + .label {
+  background-color: orange;
+} */
+</style>
